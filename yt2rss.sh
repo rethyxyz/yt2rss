8943c7702faa3bbea2dbed18fb5a70ca15eb5e1c
Brody Rethy <brodyrethy@gmail.com>
2021-03-26 19:03:07 -0300

    updated

--- yt2rss.sh
+++ yt2rss.sh
@@ -1,4 +1,4 @@
-# Check if args given
+# Check for args
 if [ $# -eq 0 ]; then echo ":: No link to channel(s) given"; exit 1; fi
 
 URLS=( "$@" )
@@ -8,3 +8,5 @@ do
 	CHANNEL_ID=$(curl -s "$URL" | sed -e 's/[{}]/''/g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | grep -m 1 'channelId' | sed -e 's/header\"\:\"c4TabbedHeaderRenderer\"\:\"channelId\"\:\"//g' -e 's/\"//g' -e 's/content\:horizontalListRenderer\:items\:\[gridChannelRenderer\:channelId\://g' -e 's/content\:expandedShelfContentsRenderer\:items\:\[channelRenderer\:channelId\://g')
 	echo "https://www.youtube.com/feeds/videos.xml?channel_id=$CHANNEL_ID"
 done
+
+exit 0
